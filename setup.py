93ac8e1d2f0b94ed61ac22d91768c46387674aed
KIvoy <skytianzhu@gmail.com>
2023-03-08 14:41:39 -0500

    updated version to 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nikke-assistant',
-    version='1.0',
+    version='1.1',
     packages=find_packages(include=['nikke-assistant', 'nikke-assistant.*'])
 )
 
