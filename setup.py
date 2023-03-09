a400746510da6ec4cee745a248603321c8f56866
KIvoy <skytianzhu@gmail.com>
2023-03-09 17:51:11 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nikke-assistant',
-    version='1.2',
+    version='1.2.1',
     packages=find_packages(include=['nikke-assistant', 'nikke-assistant.*'])
 )
 
